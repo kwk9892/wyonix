792a7875f3a4d7f659115aaab459ad9a27fadbd7
kwk9892 <kwk9892@gmail.com>
2019-09-30 01:31:24 +0800

    New

---
+++ wp-content/plugins/akismet/views/get.php
@@ -0,0 +1,2 @@
+<form name="akismet_activate" action="https://akismet.com/get/" method="POST" target="_blank">
+	<input type="hidden" name="passback_url" value="
---
+++ wp-content/themes/twentysixteen/searchform.php
@@ -0,0 +1,2 @@
+
+<form role="search" method="get" class="search-form" action="
